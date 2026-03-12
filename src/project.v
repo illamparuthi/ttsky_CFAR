49d51c3b3f5d7b346ff08c06e4700100008b2785
illamparuthi <116744508+illamparuthi@users.noreply.github.com>
2026-03-12 12:36:05 +0530

    Modify module declaration for ttsky_CFAR

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module ttsky_CFAR (
+module tt_um_ttsky_CFAR (
     input  wire [7:0] ui_in,    // Dedicated inputs (radar samples)
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path (unused)
