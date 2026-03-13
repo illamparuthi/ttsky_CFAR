01d42c37feedb9e4d683b2806309bc6bad51b132
illamparuthi <116744508+illamparuthi@users.noreply.github.com>
2026-03-13 15:54:30 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,52 +1,32 @@
 `default_nettype none
-`timescale 1ns / 1ps
+`timescale 1ns/1ps
 
-/* TinyTapeout testbench
- * Instantiates the DUT and exposes signals for cocotb.
- */
+module tb();
 
-module tb ();
+reg clk;
+reg rst_n;
+reg ena;
+reg [7:0] ui_in;
+reg [7:0] uio_in;
 
-  // Dump waveform
-  initial begin
-    $dumpfile("tb.fst");
-    $dumpvars(0, tb);
-    #1;
-  end
+wire [7:0] uo_out;
+wire [7:0] uio_out;
+wire [7:0] uio_oe;
 
-  // Inputs
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
+tt_um_ttsky_cfar dut(
+    .ui_in(ui_in),
+    .uo_out(uo_out),
+    .uio_in(uio_in),
+    .uio_out(uio_out),
+    .uio_oe(uio_oe),
+    .ena(ena),
+    .clk(clk),
+    .rst_n(rst_n)
+);
 
-  // Outputs
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
-
-  // Instantiate TinyTapeout wrapper
-  tt_um_ttsky_cfar user_project (
-
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
-
-      .ui_in  (ui_in),
-      .uo_out (uo_out),
-      .uio_in (uio_in),
-      .uio_out(uio_out),
-      .uio_oe (uio_oe),
-      .ena    (ena),
-      .clk    (clk),
-      .rst_n  (rst_n)
-  );
+initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+end
 
 endmodule
